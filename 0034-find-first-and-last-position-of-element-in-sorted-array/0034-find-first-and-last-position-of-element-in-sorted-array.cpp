70564e485b9395aa4b9e31dca2da7fbbe11b1a90
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-06-30 10:56:27 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -10,17 +10,16 @@ public:
             int mid = (low + high) / 2;
             if(nums[mid] == target)
             {
-                ans =  mid;
+                ans = mid;
                 high = mid - 1;
             }
-            else if(target < nums[mid])
+            else if(nums[mid] > target)
                 high = mid - 1;
             else
                 low = mid + 1;
         }
         return ans;
     }
-
     int lastoccurence(vector<int>& nums, int target)
     {
         int low = 0;
@@ -34,7 +33,7 @@ public:
                 ans = mid;
                 low = mid + 1;
             }
-            else if(target < nums[mid])
+            else if(nums[mid] > target)
                 high = mid - 1;
             else
                 low = mid + 1;
