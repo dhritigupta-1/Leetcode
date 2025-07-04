417adc0e20877c37a2bff08d9c187921041bcf69
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-04 13:06:36 +0530

    Time: 7 ms (97.78%), Space: 56.8 MB (69.02%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,36 @@
+class Solution {
+    boolean Palindrome(String s, int start, int i)
+    {
+        while(start < i)
+        {
+            if(s.charAt(start) != s.charAt(i))
+                return false;
+            start++;
+            i--;
+        }
+        return true;
+    }
+    void Func(String s, List<String> temp, List<List<String>> res, int start)
+    {
+        if(start == s.length())
+        {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=start; i<s.length(); i++)
+        {
+            if(Palindrome(s,start,i))
+            {
+                temp.add(s.substring(start, i+1));
+                Func(s, temp, res, i+1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        List<String> temp = new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
+        Func(s, temp, res, 0);
+        return res;
+    }
+}
