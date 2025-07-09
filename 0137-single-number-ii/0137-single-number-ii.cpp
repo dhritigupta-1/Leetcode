dfd54afff58a7b269dfebeafef7abc2f7c08bd46
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-09 09:59:33 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.45%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int a = 0, b = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            a ^= nums[i] & ~b;
+            b ^= nums[i] & ~a;
+        }
+        return a;
+    }
+};
