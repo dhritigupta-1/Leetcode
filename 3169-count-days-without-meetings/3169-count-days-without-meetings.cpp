620b694a5066fe96e1f463721d8152f8d03de7bc
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-11 19:23:38 +0530

    Time: 76 ms (35.98%), Space: 126.7 MB (38.18%) - LeetHub

---
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(),meetings.end());
+        int days_worked = 0;
+        vector<vector<int>> result;
+        if (meetings.empty()) return 0;
+        result.push_back(meetings[0]);
+        for (int i = 1; i < meetings.size(); i++) {
+            vector<int>& last = result.back();
+            if (meetings[i][0] <= last[1]) {
+                last[1] = max(last[1], meetings[i][1]);
+            } else {
+                result.push_back(meetings[i]);
+            }
+        }
+        for(int i=0;i<result.size();i++){
+            days_worked += ((result[i][1]+1)-result[i][0]);
+        }
+        return days-days_worked;
+    }
+};
