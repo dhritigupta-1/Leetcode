b0e428186f4a9fa27192101b646dca98cdff2095
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-19 19:36:19 +0530

    Time: 10 ms (19.7%), Space: 45.8 MB (67.63%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int r = maze.length;
+        int c = maze[0].length;
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+        int count = 0;
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        q.add(new Pair<>(entrance[0],entrance[1]));
+        maze[entrance[0]][entrance[1]] = '+';
+        while(q.size() > 0)
+        {
+            int size = q.size();
+            for(int i=0; i<size; i++)
+            {
+                Pair<Integer,Integer> node = q.poll();
+                for(int j=0; j<4; j++)
+                {
+                    int x = node.getKey() + dx[j];
+                    int y = node.getValue() + dy[j];
+                    if(x>=0 && y>=0 && x<r && y<c && maze[x][y]=='.')
+                    {
+                        if((x==0 || y==0 || x==r-1 || y==c-1) && !(x == entrance[0] && y== entrance[1]))
+                            return count+1;
+                        q.add(new Pair<>(x,y));
+                        maze[x][y]='+';
+                    }
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
+}
