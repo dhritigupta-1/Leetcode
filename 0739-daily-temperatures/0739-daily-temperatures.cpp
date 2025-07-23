96dc4ee4a32a42059f1c1edec3cc28b9364075cd
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-23 12:48:02 +0530

    Time: 31 ms (27.34%), Space: 107.5 MB (19.88%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> max(temperatures.size(),0);
+        stack<int> st;
+        for(int i=0; i<temperatures.size(); i++)
+        {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int prev = st.top();
+                st.pop();
+                max[prev] = i - prev;
+            }
+            st.push(i);
+        }
+        return max;
+    }
+};
