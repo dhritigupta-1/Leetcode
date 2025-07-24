75c175f8a1a9b25382be8a803a2de79724b5f18b
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-24 18:37:43 +0530

    Time: 8 ms (80.89%), Space: 11.8 MB (53.24%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -13,8 +13,8 @@ public:
         ListNode* fast = head;
         while(fast != NULL && fast->next != NULL)
         {
-            slow = slow -> next;
-            fast = fast -> next -> next;
+            slow = slow->next;
+            fast = fast->next->next;
             if(slow == fast)
                 return true;
         }
