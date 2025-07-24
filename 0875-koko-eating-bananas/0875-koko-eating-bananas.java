c951229bee46ac467131b11a9c873dcc323c25a5
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-24 12:55:03 +0530

    Time: 25 ms (39.23%), Space: 45.7 MB (5.18%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -4,7 +4,7 @@ class Solution {
         int hour = 0;
         for(int i=0; i<piles.length; i++)
         {
-            hour += Math.ceil((piles[i]*1.0) / mid);
+            hour += Math.ceil((piles[i]* 1.0) / mid);
         }
         if(hour <= h)
             return true;
