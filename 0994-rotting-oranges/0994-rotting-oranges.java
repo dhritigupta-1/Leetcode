3aa093ff3ed94e4fa264f97c7cfe2e581bfdd11d
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2025-07-25 19:34:38 +0530

    Time: 2 ms (67.74%), Space: 42.7 MB (38.43%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,7 +2,7 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int r = grid.length;
         int c = grid[0].length;
-        int fresh=0, rotten=0, min=0, ans;
+        int fresh=0, rotten=0, min=0;
         int[] dx = {0,0,1,-1};
         int[] dy = {1,-1,0,0};
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
@@ -15,7 +15,7 @@ class Solution {
                 else if(grid[i][j] == 2)
                 {
                     rotten++;
-                    q.add(new Pair<>(i, j));
+                    q.add(new Pair<>(i,j));
                 }
             }
         }
@@ -31,13 +31,13 @@ class Solution {
                     int y = node.getValue() + dy[j];
                     if(x>=0 && y>=0 && x<r && y<c && grid[x][y] == 1)
                     {
-                        q.add(new Pair<>(x, y));
-                        grid[x][y] = 2;
+                        grid[x][y] = -1;
+                        q.add(new Pair<>(x,y));
                         fresh--;
                     }
                 }
             }
-            if(!q.isEmpty()) min++;
+            if(q.size() > 0) min++;
         }
         if(fresh == 0) return min;
         return -1;
