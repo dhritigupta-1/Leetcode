feb76f97183b887aa95beb612004253a79f56966
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-22 16:35:28 +0530

    Time: 43 ms (25.48%), Space: 14.1 MB (80.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> map;
+        // unordered_map<int,int> map;
+        // int sum = 0;
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     sum = target - nums[i];
+        //     if(map.find(sum) != map.end())
+        //         return {map[sum], i};
+        //     map[nums[i]] = i; 
+        // }
+        // return {};
+
+        // OR
         int sum = 0;
         for(int i=0; i<nums.size(); i++)
         {
-            sum = target - nums[i];
-            if(map.find(sum) != map.end())
-                return {map[sum], i};
-            map[nums[i]] = i; 
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                sum = nums[i] + nums[j];
+                if(sum == target)
+                    return vector<int> {i,j};
+            }
         }
-        return {};
+        return vector<int>{};
     }
 };
