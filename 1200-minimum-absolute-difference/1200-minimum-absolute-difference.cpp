7cec0130d15ce2c8501eab111c49492ce5042054
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-26 13:08:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -6,8 +6,6 @@ public:
         vector<vector<int>> ans;
         for(int i=0; i<arr.size()-1; i++){
             diff = min(diff, arr[i+1]-arr[i]);
-        }
-        for(int i=0; i<arr.size()-1; i++){
             if(arr[i+1]- arr[i] == diff) ans.push_back({arr[i], arr[i+1]});
         }
         return ans;
