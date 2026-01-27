67e884fe82fd28d6bdef60c159057c2083e569f8
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-27 20:12:22 +0530

    Time: 5 ms (20.01%), Space: 8.5 MB (53.84%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sum = 0, r = 0;
+        while(x != 0)
+        {
+            r = x % 10;
+            if((sum > INT_MAX/10) || (sum < INT_MIN/10))
+                return 0;
+            sum = (sum * 10) + r;
+            x = x / 10;
+        }
+        return sum;
+    }
+};
