49f952226d8bc5abe3972c8ad3f889888941be8c
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-28 14:40:16 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, lead = 0;
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(lead == 0)
-                candidate = nums[i];
-            if(candidate == nums[i])
-                lead++;
-            else
-                lead--;
+        unordered_map<int, int> map;
+        int lead = 0;
+        for(auto f : nums)
+            map[f]++;
+        for(auto f : nums){
+            if(map[f] > nums.size() / 2)
+                lead = f;
         }
-        return candidate;
+        return lead;
     }
 };
