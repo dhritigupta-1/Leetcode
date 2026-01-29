8f6afc31bc3ada32ed93008752fdc69f814422a7
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-29 22:09:40 +0530

    Time: 5 ms (30.18%), Space: 18.3 MB (26.63%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -10,7 +10,7 @@ public:
         sort(s.begin(), s.end(), [](const auto a, const auto b){
             if(a.second != b.second)
                 return a.second > b.second;
-            return a.second < b.second;
+            return a.first < b.first;
         });
         vector<string> ans;
         for(int i=0; i<k; i++)
