2aa2620b4207e3bd567df30f22bf6e86367a187b
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-01-29 22:09:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string, int> map;
+        for(auto ch : words)
+            map[ch]++;
+        vector<pair<string, int>> s;
+        for(auto p : map)
+            s.push_back(p);
+        sort(s.begin(), s.end(), [](const auto a, const auto b){
+            if(a.second != b.second)
+                return a.second > b.second;
+            return a.second < b.second;
+        });
+        vector<string> ans;
+        for(int i=0; i<k; i++)
+            ans.push_back(s[i].first);
+        return ans;
+    }
+};
