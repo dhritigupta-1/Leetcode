445de5062d5c7bfb73a5f45c69c214cab119260b
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-05 14:13:52 +0530

    Time: 0 ms (100%), Space: 11.8 MB (78.91%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> map;
+        for(auto it : arr) map[it]++;
+        vector<int> ans;
+        for(auto i : map) ans.push_back(i.second);
+        for(int i=0; i<ans.size(); i++){
+            for(int j=i+1; j<ans.size(); j++) {
+                if(ans[i] == ans[j]) return false;
+            }
+        }
+        return true;
+    }
+};
