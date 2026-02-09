6fb93d4738d804d1ef2959610ae571bd9a210a34
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-09 12:06:26 +0530

    Time: 0 ms (100%), Space: 27.2 MB (36.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> st;
+        int n = height.size();
+        stack<int> s1;
         int water = 0;
-        for(int i=0; i<height.size(); i++){
-            while(!st.empty() && height[i] > height[st.top()]){
-                int mid = st.top();
-                st.pop();
-                if(st.empty()) break;
-                int left = st.top();
+        for(int i=0; i<n; i++){
+            while(!s1.empty() && height[i] > height[s1.top()]){
+                int mid = s1.top();
+                s1.pop();
+                if(s1.empty()) break;
+                int left = s1.top();
                 int width = i - left - 1;
                 int boundedheight = min(height[i], height[left]) - height[mid];
                 water += width * boundedheight;
             }
-            st.push(i);
+            s1.push(i);
         }
         return water;
     }
