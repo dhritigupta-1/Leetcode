d6ab34fc512f5c741ff9a62257ed1bc8222cb08f
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-14 11:30:14 +0530

    Time: 147 ms (8.91%), Space: 149 MB (8.06%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    bool Palindrome(string s, int low, int high)
-    {
-        while(low < high)
-        {
-            if(s[low] != s[high])
-                return false;
+    bool Palindrome(string s, int low, int high){
+        while(low < high){
+            if(s[low] != s[high]) return false;
             low++;
             high--;
         }
         return true;
     }
-    void Func(string s, vector<string>&temp, vector<vector<string>>& res, int start)
-    {
-        if(start == s.length())
-        {
+    void Func(string s, vector<string> temp, vector<vector<string>>& res, int start){
+        if(start == s.length()){
             res.push_back(temp);
             return;
         }
-        for(int i=start; i<s.length(); i++)
-        {
-            if(Palindrome(s, start, i))
-            {
-                temp.push_back(s.substr(start, i - start + 1));
+        for(int i=start; i<s.size(); i++){
+            if(Palindrome(s, start, i)){
+                temp.push_back(s.substr(start, i - start + 1)); // choice
                 Func(s, temp, res, i+1);
-                temp.pop_back();
+                temp.pop_back(); // bactracking
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
         vector<string> temp;
+        vector<vector<string>> res;
         Func(s, temp, res, 0);
         return res;
     }
