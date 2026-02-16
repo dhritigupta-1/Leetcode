e37d9c0450fd197f4f25e02566b825bc425cfd8d
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-16 23:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (27.07%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector<string> res;
+    vector<string> ans;
     unordered_map<char, string> map;
-    void Func(string digits, string current, int start){
-        if(start == digits.size()){
-            res.push_back(current);
+    void func(string digits, string current, int start){
+        if(start == digits.length()){
+            ans.push_back(current);
             return;
         }
-        string letters = map[digits[start]];
-        for(char ch : letters) Func(digits, current+ch, start+1);
+        string letter = map[digits[start]];
+        for(char ch : letter) func(digits, current+ch, start+1);
     }
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {};
         map = {
-            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},
-            {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, 
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
         };
-        Func(digits, "", 0);
-        return res;
+        func(digits, "", 0);
+        return ans;
+        
     }
 };
