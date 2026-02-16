7c282dd1708db778a3403414611e7c7bc8dd7061
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-16 10:43:27 +0530

    Time: 0 ms (100%), Space: 12.7 MB (96.01%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,23 +11,28 @@
  */
 class Solution {
 public:
-    void helper(vector<int>& ans, TreeNode* root)
-    {
-        if(root == NULL)
-        {
-            ans.push_back(INT_MAX);
-            return;
-        }
-        ans.push_back(root->val);
-        helper(ans, root->left);
-        helper(ans, root->right);
-    }
+    // void helper(vector<int>& ans, TreeNode* root)
+    // {
+    //     if(root == NULL)
+    //     {
+    //         ans.push_back(INT_MAX);
+    //         return;
+    //     }
+    //     ans.push_back(root->val);
+    //     helper(ans, root->left);
+    //     helper(ans, root->right);
+    // }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> ans1, ans2;
-        helper(ans1, p);
-        helper(ans2, q);
-        if(ans1 == ans2)
-            return true;
-        return false;
+    //     vector<int> ans1, ans2;
+    //     helper(ans1, p);
+    //     helper(ans2, q);
+    //     if(ans1 == ans2)
+    //         return true;
+    //     return false;
+    // }
+    if( p == NULL && q == NULL) return true;
+    if( p == NULL || q == NULL) return false;
+    if(p->val != q->val) return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
