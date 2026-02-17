23c997d81ff225daec6afad241d32bbdfe985562
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-17 10:53:06 +0530

    Time: 0 ms (100%), Space: 16.3 MB (41.42%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -24,6 +24,16 @@ public:
         // }
         // return grid[n-1][m-1];
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return Func(grid, n-1, m-1, dp);
+        //return Func(grid, n-1, m-1, dp);
+        dp[0][0] = grid[0][0];
+        for(int row=0; row<n; row++){
+            for(int col=0; col<m; col++){
+               if(row==0 && col==0) continue;
+               int top = (row > 0) ? dp[row-1][col] : INT_MAX;
+               int left = (col > 0) ? dp[row][col-1] : INT_MAX;
+               dp[row][col] = grid[row][col] + min(top, left);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
