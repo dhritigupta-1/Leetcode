12c74cb9d7bb13a48446226e3385554b7bb8c375
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-17 10:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int func(int n, vector<int>& dp){
-        if(n == 0 || n == 1) return dp[n] = n;
+        //if(n == 0 || n == 1) return dp[n] = n;
         // int x;
         // if(dp[n-1] != -1) x = dp[n-1];
         // else x = func(n-1, dp);
@@ -9,11 +9,19 @@ public:
         // if(dp[n-2] != -1) y = dp[n-2];
         // else y = func(n-2, dp);
 
-        // OR
-        if(dp[n-1] != -1) return dp[n];
-        int x = func(n-1, dp);
-        int y = func(n-2, dp);
-        return dp[n] = x + y;
+        // OR (Memoriztion)
+        // if(dp[n-1] != -1) return dp[n];
+        // int x = func(n-1, dp);
+        // int y = func(n-2, dp);
+        // return dp[n] = x + y;
+
+        // OR (Tabulation)
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
     int fib(int n) {
         // if(n == 0 || n == 1)
@@ -21,6 +29,7 @@ public:
         // return fib(n - 1) + fib(n - 2);
 
         // using dp
+        if(n <= 1) return n;
         vector<int> dp(n+1, -1);
         return func(n, dp);
     }
