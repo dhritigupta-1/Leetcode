57d248c01494ad8454d85da77c0badf53944c34a
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-19 15:39:36 +0530

    Time: 3 ms (28.07%) | Memory: 16.8 MB (56.43%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        vector<int>dx = {0,0,1,-1};
+        vector<int>dy = {1,-1,0,0};
+        queue<pair<int, int>> q;
+        int min = 0, fresh = 0, rotten = 0;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2){
+                    rotten++;
+                    q.push({i,j});
+                }
+            }
+        }
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                pair<int, int> node = q.front(); q.pop();
+                for(int j=0; j<4; j++){
+                    int x = node.first + dx[j];
+                    int y = node.second +dy[j];
+                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==1){
+                        grid[x][y] = 2;
+                        fresh--;
+                        q.push({x,y});
+                    }
+                }
+            }
+            if(q.size()) min++;
+        }
+        if(fresh == 0) return min;
+        return -1;
+    }
+};
