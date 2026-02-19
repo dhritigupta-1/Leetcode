4eb0690d8448683db925a0a9e1b788eff8af675a
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-19 10:52:54 +0530

    Time: 25 ms (72.01%) | Memory: 16 MB (99.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, vector<vector<bool>>& vis, int i, int j, int n, int m){
-        if(i<0 || j<0 || j>=m || i>=n || grid[i][j] == '0' || vis[i][j]) return;
-        //grid[i][j] = '0'; // mark as visited
-        vis[i][j] = true;
+    void DFS(vector<vector<char>>& grid, int i, int j, int n, int m){
+        if(i<0 || j<0 || j>=m || i>=n || grid[i][j] == '0') return; // || vis[i][j]
+        grid[i][j] = '0'; // mark as visited
+        // vis[i][j] = true; // not optimized solution as using another 2D array 
         // chheck all 4 directions
-        DFS(grid, vis, i-1, j, n, m);
-        DFS(grid, vis, i, j-1, n, m);
-        DFS(grid, vis, i, j+1, n, m);
-        DFS(grid, vis, i+1, j, n, m);
+        DFS(grid, i-1, j, n, m);
+        DFS(grid, i, j-1, n, m);
+        DFS(grid, i, j+1, n, m);
+        DFS(grid, i+1, j, n, m);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int ans = 0;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        //vector<vector<bool>> vis(n, vector<bool>(m, false));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    DFS(grid, vis, i, j, n, m);
+                if(grid[i][j] == '1' ){ // && !vis[i][j]
+                    DFS(grid, i, j, n, m);
+                    //DFS(grid, vis, i, j, n, m);
                     ans++;
                 }
             }
