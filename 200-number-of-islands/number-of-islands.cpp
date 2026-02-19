d108c169ea52cb3b7a0e0209fd72cf52b0e2a099
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-19 11:00:08 +0530

    Time: 32 ms (25.46%) | Memory: 23.7 MB (22.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    void DFS(vector<vector<char>>& grid, int i, int j, int n, int m){
-        if(i<0 || j<0 || j>=m || i>=n || grid[i][j] == '0') return; // || vis[i][j]
-        grid[i][j] = '0'; // mark as visited
-        // vis[i][j] = true; // not optimized solution as using another 2D array 
-        // chheck all 4 directions
-        DFS(grid, i-1, j, n, m);
-        DFS(grid, i, j-1, n, m);
-        DFS(grid, i, j+1, n, m);
-        DFS(grid, i+1, j, n, m);
+    void BFS(vector<vector<char>>& grid, int i, int j, int n, int m){
+        queue<pair<int, int>> q;
+        q.push({i,j});
+        grid[i][j] = '0'; // mark visited
+        vector<pair<int, int>> directions = {{1,0}, {0,1}, {-1, 0}, {0,-1}};
+        while(!q.empty()){
+            auto[x, y] = q.front(); q.pop();
+            for(auto [dx, dy] : directions){
+                int newX = x + dx, newY = y + dy;
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == '1'){
+                    q.push({newX, newY});
+                    grid[newX][newY] = '0'; // mark visited
+                }
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int ans = 0;
-        //vector<vector<bool>> vis(n, vector<bool>(m, false));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == '1' ){ // && !vis[i][j]
-                    DFS(grid, i, j, n, m);
-                    //DFS(grid, vis, i, j, n, m);
+                if(grid[i][j] == '1' ){ 
+                    BFS(grid, i, j, n, m);
                     ans++;
                 }
             }
