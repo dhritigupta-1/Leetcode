0e8c41f80ca5fe50978bfc675de62ca03f189abe
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-19 15:52:05 +0530

    Time: 3 ms (28.07%) | Memory: 17.2 MB (31.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,15 +3,15 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
+        vector<vector<int>> vis = grid;
         vector<int>dx = {0,0,1,-1};
         vector<int>dy = {1,-1,0,0};
         queue<pair<int, int>> q;
-        int min = 0, fresh = 0, rotten = 0;
+        int min = 0, fresh = 0;
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
-                if(grid[i][j] == 1) fresh++;
-                else if(grid[i][j] == 2){
-                    rotten++;
+                if(vis[i][j] == 1) fresh++;
+                else if(vis[i][j] == 2){
                     q.push({i,j});
                 }
             }
@@ -22,9 +22,9 @@ public:
                 pair<int, int> node = q.front(); q.pop();
                 for(int j=0; j<4; j++){
                     int x = node.first + dx[j];
-                    int y = node.second +dy[j];
-                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==1){
-                        grid[x][y] = 2;
+                    int y = node.second + dy[j];
+                    if(x>=0 && y>=0 && x<r && y<c && vis[x][y]==1){
+                        vis[x][y] = 2;
                         fresh--;
                         q.push({x,y});
                     }
