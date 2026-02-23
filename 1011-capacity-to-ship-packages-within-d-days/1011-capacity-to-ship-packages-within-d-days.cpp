e3eb0f161a2859f15ef079adf6c18f7c1b140878
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-23 12:07:00 +0530

    Time: 16 ms (20.66%), Space: 35.1 MB (53.56%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    bool Func(vector<int>& weights, int days, int mid)
-    {
+    bool Func(vector<int>& weights, int days, int mid){
         int cap = 0;
         int d = 1;
-        for(int i=0; i<weights.size(); i++)
-        {
-            if(cap+weights[i] <= mid)
-                cap += weights[i];
-            else
-            {
+        for(int i=0; i<weights.size(); i++){
+            if(cap+weights[i] <= mid) cap += weights[i];
+            else{
                 cap = weights[i];
                 d++;
             }
         }
-        if(d <= days)
-                return true;
-         return false;
+        if(d <= days) return true;
+        return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int sum = 0;
-        int l = 0;
-        for(int i=0; i<weights.size(); i++)
-        {
-            sum += weights[i];
-            if(weights[i] > l)
-                l = weights[i];
-        }
-        int low = l;
+        for(int i=0; i<weights.size(); i++) sum+=weights[i];
+        int low = *max_element(weights.begin(), weights.end());
         int high = sum;
         int ans = 0;
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
-            if(Func(weights, days, mid))
-            {
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(Func(weights, days, mid)){
                 ans = mid;
-                high = mid - 1;
+                high = mid-1;
             }
-            else
-                low = mid + 1;
+            else low = mid+1;
         }
         return ans;
     }
