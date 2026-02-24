f75717e29ea0ea357ebaed3a63882efcbae48cc2
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-24 09:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (8.76%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         vector<int> ans;
         int num = 1, i=0;
-        while(ans.size()<k){
+        while(ans.size()<=k){
             if(i < arr.size() && arr[i] == num)
                 i++;
             else ans.push_back(num);
