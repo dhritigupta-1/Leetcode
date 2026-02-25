bbcca79c1936cf9fde68ac3620fc621585abc99f
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-02-25 09:15:30 +0530

    Time: 29 ms (42.55%) | Memory: 23.6 MB (24.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,15 +3,16 @@ public:
     void BFS(vector<vector<char>>& grid, int i, int j, int n, int m){
         queue<pair<int, int>> q;
         q.push({i,j});
-        grid[i][j] = '0'; // mark visited
-        vector<pair<int, int>> directions = {{1,0}, {0,1}, {-1, 0}, {0,-1}};
+        vector<pair<int, int>> directions = {{0,1}, {1,0}, {0,-1}, {-1, 0}};
         while(!q.empty()){
-            auto[x, y] = q.front(); q.pop();
+            auto node = q.front();
+            q.pop();
             for(auto [dx, dy] : directions){
-                int newX = x + dx, newY = y + dy;
-                if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == '1'){
+                int newX = node.first + dx;
+                int newY = node.second + dy;
+                if(newX>=0 && newX<n && newY>=0 && newY<m && grid[newX][newY]=='1'){
+                    grid[newX][newY] = '0';
                     q.push({newX, newY});
-                    grid[newX][newY] = '0'; // mark visited
                 }
             }
         }
@@ -22,7 +23,7 @@ public:
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == '1' ){ 
+                if(grid[i][j]=='1'){
                     BFS(grid, i, j, n, m);
                     ans++;
                 }
