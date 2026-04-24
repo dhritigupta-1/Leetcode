28fd57a85bacfa543a85884fc55254622b72455e
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-04-24 23:17:57 +0530

    Time: 940 ms (53.23%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.salary as Salary
+from (
+    select *, dense_rank() over (partition by departmentId order by salary desc) as rnk from Employee
+) e join department d on e.departmentId = d.id where rnk <= 3;
