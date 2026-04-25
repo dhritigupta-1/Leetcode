85ab6bc0ee0f0d8a2c6d29f356a5c320c4c80887
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-04-26 01:30:54 +0530

    Time: 388 ms (17.62%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+with daily as ( select visited_on, sum(amount) as daily_amount from Customer group by visited_on)
+select visited_on, sum(daily_amount) over (order by visited_on rows between 6 preceding and current row) as amount, round(sum(daily_amount) over( order by visited_on rows between 6 preceding and current row) / 7, 2) as average_amount from daily order by visited_on limit 100000 offset 6;
