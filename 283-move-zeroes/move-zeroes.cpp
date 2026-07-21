2cb20073a691441ded67e19772bfe6c481e70c26
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-07-21 16:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (56.43%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0, j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0) nums[j++] = nums[i];
+            else count++;
+        }
+        for(int i=0; i<count; i++){
+            nums[n-1-i] = 0;
+        }
+    }
+};
