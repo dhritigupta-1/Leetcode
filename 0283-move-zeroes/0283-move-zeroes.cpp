f16eaec3e6dd74ebdafaef1801722f738aa8d3a1
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-07-21 16:17:34 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
-        int j = 0;
         int n = nums.size();
-        for(int i=0; i<n; i++)
-        {
-            if(nums[i] != 0)
-                nums[j++] = nums[i];
-            else
-                count++;
+        int count = 0, j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0) nums[j++] = nums[i];
+            else count++;
         }
-        for(int i = 0; i<count; i++)
-        {
-            nums[n - 1 - i] = 0;
+        for(int i=0; i<count; i++){
+            nums[n-1-i] = 0;
         }
     }
 };
