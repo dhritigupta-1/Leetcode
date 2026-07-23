29c36ef18fbff00b283bd6e0a519aeef94cf60b0
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-07-23 16:15:33 +0530

    Time: 0 ms (100%), Space: 15 MB (20.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -10,18 +10,7 @@ public:
             }
             map[nums[i]] = i;
         }
-        return{};
-        // OR
-        /* int sum = 0;
-        for(int i=0; i<nums.size(); i++)
-        {
-            for(int j=i+1; j<nums.size(); j++)
-            {
-                sum = nums[i] + nums[j];
-                if(sum == target)
-                    return vector<int> {i,j};
-            }
-        }
-        return vector<int>{}; */
+        return {};
     }
 };
+
