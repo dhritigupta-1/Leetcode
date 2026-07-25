9d3e850166dbf4b9bd203f923f8d8b4b9d8c2767
Dhriti Gupta <149320960+dhritigupta-1@users.noreply.github.com>
2026-07-25 14:46:04 +0530

    Time: 47 ms (64.59%), Space: 29.2 MB (29.99%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,17 +6,20 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0; i<n-2; i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-            int low = i + 1; int high = n - 1;
+            int low = i+1, high = n-1;
             while(low < high){
                 int sum = nums[i] + nums[low] + nums[high];
-                if(sum == 0 && i!=low && i!=high && low!=high) {
+                if(sum == 0){
                     ans.push_back({nums[i], nums[low], nums[high]});
                     low++; high--;
+                    while(low < high && nums[low] == nums[low-1]) low++;
+                    while(low < high && nums[high] == nums[high+1]) high--;
                 }
-                else if( sum > 0) high--;
+                else if(sum > 0) high--;
                 else low++;
             }
         }
         return ans;
     }
 };
+
